d8cbd6b86ae637459e1862635772a08283d954f8
PKJ11 <108004611+PKJ11@users.noreply.github.com>
2025-05-19 16:13:52 +0530

    second commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:8080' // Adjust based on your frontend URL
+  origin: '*'
 }));
 app.use(express.json());
 
@@ -40,8 +40,8 @@ app.post('/api/send-email', async (req, res) => {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASSWORD,
+        user: process.env.GMAIL_USER || "pratikkumarjhavnit@gmail.com",
+        pass: process.env.GMAIL_PASSWORD || "apvp cjeq ljax nurr",
       },
     });
 
